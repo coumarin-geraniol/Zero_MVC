28bc197ebb8b93b96f449d5de3e7c23f4850549d
coumarin-geraniol <semahatartikova@gmail.com>
2023-06-01 02:01:29 +0300

    third commit, after first identity

--- ZeroWeb/wwwroot/js/product.js
+++ ZeroWeb/wwwroot/js/product.js
@@ -1,17 +1,54 @@
-﻿
+﻿var dataTable;
 $(document).ready(function () {
     loadDataTable();
 });
 
 function loadDataTable() {
-    dataTable = $('#tblTable').DataTable({
+    dataTable = $('#tblData').DataTable({
         "ajax": { url: '/admin/product/getall' },
         "columns": [
-            { data: 'name', "width": "15%" },
-            { data: 'position', "width": "15%" },
-            { data: 'salary', "width": "15%" },
-            { data: 'office', "width": "15%" }
+            { data: 'title', "width": "20%" },
+            { data: 'author', "width": "18%" },
+            { data: 'category.name', "width": "12%" },
+            { data: 'isbn', "width": "15%" },
+            { data: 'listPrice', "width": "10%" },
+            {
+                data: 'id',
+                "render": function (data) {
+                    return `<div class="w-75 btn-group" role="group">
+                     <a href="/admin/product/upsert?id=${data}" class="btn btn-primary mx-2 opacity-75"> <i class="bi bi-pencil-square"></i> Edit</a>               
+                     <a onClick=Delete('/admin/product/delete/${data}') class="btn btn-danger mx-2 opacity-75"> <i class="bi bi-trash-fill"></i> Delete</a>
+                    </div>`
+                },
+                "width": "30%"
+            }
         ]
     });
 }
 
+function Delete(url) {
+    Swal.fire({
+        title: 'Are you sure?',
+        text: "You won't be able to revert this!",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#d33d33BF',
+        cancelButtonColor: '#292B2CBF',
+        confirmButtonText: 'Yes, delete it!',
+        customClass: {
+            confirmButton: 'right-gap',
+        }
+    }).then((result) => {
+        if (result.isConfirmed) {
+            $.ajax({
+                url: url,
+                type: 'DELETE',
+                success: function (data) {
+                    dataTable.ajax.reload(),
+                    toastr.success(data.message);   
+                }
+            })
+        }
+    })
+}
+  
